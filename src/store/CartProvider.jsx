067750d47288f9181cf2403993cfad2582e7a12b
Russkazkin <russkazkin@gmail.com>
2022-03-19 05:50:53 +0300

    Add Cart context provider

---
+++ src/store/CartProvider.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import CartContext from './cartContext';
+
+const CartProvider = ({ children }) => {
+  const addItemToCartHandler = item => {};
+  const removeItemFromCartHandler = id => {};
+  const cartContext = {
+    items: [],
+    totalAmount: 0,
+    addItem: addItemToCartHandler,
+    removeItem: removeItemFromCartHandler,
+  };
+  return (
+    <CartContext.Provider value={cartContext}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export default CartProvider;
