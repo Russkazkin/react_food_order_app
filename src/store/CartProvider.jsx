65cd74d8eebcadedaf61cc2a23dde63326a792c2
Russkazkin <russkazkin@gmail.com>
2022-03-30 05:39:36 +0300

    Add cartReducer base structure

--- src/store/CartProvider.jsx
+++ src/store/CartProvider.jsx
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import CartContext from './CartContext';
 
+const ADD = 'ADD';
+const REMOVE = 'REMOVE';
+
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
+
+const cartReducer = (state, action) => {
+  switch (action.type) {
+    case ADD:
+      const updatedItems = state.items.concat(action.item);
+      const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+      return { items: updatedItems, totalAmount: updatedTotalAmount };
+    default:
+      return defaultCartState;
+  }
+};
+
 const CartProvider = ({ children }) => {
-  const addItemToCartHandler = item => {};
-  const removeItemFromCartHandler = id => {};
+  const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
+  const addItemToCartHandler = item => {
+    dispatchCartAction({ type: ADD, item });
+  };
+  const removeItemFromCartHandler = id => {
+    dispatchCartAction({ type: REMOVE, id });
+  };
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
   };
